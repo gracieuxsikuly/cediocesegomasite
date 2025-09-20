9d3960dbc42d74bbd3d756e683384d39de60c692
christianemwenge <christianemwenge@gmail.com>
2025-09-20 15:11:25 +0200

    tot

--- app/Models/Activiteprogramme.php
+++ app/Models/Activiteprogramme.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activiteprogramme extends Model
 {
-
-    protected $fillable = ['description', 'dateactivite', 
-    'emplacement', 'typeactivite', 'image1', 'image2', 'image3', 'statut', 'doyenne_id', 'paroisse_id','titre'];
+    protected $fillable = ['titre', 'description', 'dateactivite', 'emplacement', 'typeactivite',
+     'image1', 'image2', 'image3', 'statut', 'doyenne_id', 'paroisse_id'];
 }
