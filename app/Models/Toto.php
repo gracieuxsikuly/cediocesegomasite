919b6623b910b0f6d7acf0ffd8a8faf14a91e3ba
gracieuxsikuly <graciersikuly@gmail.com>
2025-09-20 14:21:51 +0200

    totoexemple

---
+++ app/Models/Toto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Toto extends Model
+{
+    protected $fillable =['titre',
+    'description',
+    'emplacement'];
+}
