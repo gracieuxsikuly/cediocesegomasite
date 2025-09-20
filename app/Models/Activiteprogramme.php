09ee108c0998f9d7757808760cfe548aa5be2e49
gracieuxsikuly <graciersikuly@gmail.com>
2025-09-20 15:27:26 +0200

    slug

--- app/Models/Activiteprogramme.php
+++ app/Models/Activiteprogramme.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Activiteprogramme extends Model
 {
     protected $fillable = ['titre', 'description', 'dateactivite', 'emplacement', 'typeactivite',
-     'image1', 'image2', 'image3', 'statut', 'doyenne_id', 'paroisse_id'];
+     'image1', 'image2', 'image3', 'statut', 'doyenne_id', 'paroisse_id','slug'];
 }
