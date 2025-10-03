76dc3796f2583bf642ce060f1ec173f5fc14da42
gracieuxsikuly <graciersikuly@gmail.com>
2025-10-03 17:54:31 +0200

    gr

---
+++ app/Livewire/Backend/NiyaMwezi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend;
+
+use Livewire\Component;
+
+class NiyaMwezi extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.niya-mwezi');
+    }
+}
