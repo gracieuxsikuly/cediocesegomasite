1d40055a3d359e38fcdd9d99130c4c9eba223f36
gracieuxsikuly <graciersikuly@gmail.com>
2025-10-09 18:54:05 +0200

    htfgfgf

--- resources/views/livewire/frontend/contact-livewire.blade.php
+++ resources/views/livewire/frontend/contact-livewire.blade.php
@@ -1,39 +1,158 @@
-<div class="contact-us-form wow fadeInUp" data-wow-delay="0.25s">
-    @if ($successMessage)
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ $successMessage }}
-            <button type="button" class="btn-close" wire:click="$set('successMessage', '')"></button>
+<div>
+    <!-- Page Header Start -->
+	<div class="page-header">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12">
+					<!-- Page Header Box Start -->
+					<div class="page-header-box">
+						<h1 class="text-anime-style-2" data-cursor="-opaque">Contact Us</h1>
+						<nav class="wow fadeInUp">
+							<ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="index-2.html">home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">contact us</li>
+							</ol>
+						</nav>
+					</div>
+					<!-- Page Header Box End -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Page Header End -->
+
+    <!-- Page Contact Us Start -->
+    <div class="page-contact-us">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <!-- Contact Information Start -->
+                    <div class="contact-information">
+                        <!-- Contact Information Title Start -->
+                        <div class="section-title">
+                            <h3 class="wow fadeInUp">contact us</h3>
+                            <h2 class="text-anime-style-2" data-cursor="-opaque">Reach Out to Our <span>Church Support</span></h2>
+                            <p class="wow fadeInUp" data-wow-delay="0.25s">Contact us for support, information, or to get involved. We're here to help and welcome you to our community.</p>
+                        </div>
+                        <!-- Contact Information Title End -->
+
+                        <!-- Contact Information List Start -->
+                        <div class="contact-info-list">
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info-item wow fadeInUp" data-wow-delay="0.25s">
+                                <!-- Icon Box Start -->
+                                <div class="icon-box">
+                                    <img src="{{asset('asset_frontend/images/icon-phone.svg')}}" alt="">
+                                </div>
+                                <!-- Icon Box End -->
+
+                                <!-- Contact Info Content Start -->
+                                <div class="contact-info-content">
+                                    <p>give us a call</p>
+                                    <h3>(+01) 789 859 654</h3>
+                                </div>
+                                <!-- Contact Info Content End -->
+                            </div>
+                            <!-- Contact Info Item End -->
+
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info-item wow fadeInUp" data-wow-delay="0.5s">
+                                <!-- Icon Box Start -->
+                                <div class="icon-box">
+                                    <img src="{{asset('asset_frontend/images/icon-mail.svg')}}" alt="">
+                                </div>
+                                <!-- Icon Box End -->
+
+                                <!-- Contact Info Content Start -->
+                                <div class="contact-info-content">
+                                    <p>Send us a message</p>
+                                    <h3>info@domain.com</h3>
+                                </div>
+                                <!-- Contact Info Content End -->
+                            </div>
+                            <!-- Contact Info Item End -->
+
+                            <!-- Contact Info Item Start -->
+                            <div class="contact-info-item wow fadeInUp" data-wow-delay="0.75s">
+                                <!-- Icon Box Start -->
+                                <div class="icon-box">
+                                    <img src="{{asset('asset_frontend/images/icon-location.svg')}}" alt="">
+                                </div>
+                                <!-- Icon Box End -->
+
+                                <!-- Contact Info Content Start -->
+                                <div class="contact-info-content">
+                                    <p>Visit our location</p>
+                                    <h3>786 Maple Avenue Smalltown, USA 67890</h3>
+                                </div>
+                                <!-- Contact Info Content End -->
+                            </div>
+                            <!-- Contact Info Item End -->
+                        </div>
+                        <!-- Contact Information List End -->
+                    </div>
+                    <!-- Contact Information End -->
+                </div>
+
+                <div class="col-lg-6">
+                    <!-- Contact Form Start -->
+                    <div class="contact-us-form wow fadeInUp" data-wow-delay="0.25s">
+                        <form id="contactForm" action="#" method="POST" data-toggle="validator" class="wow fadeInUp" data-wow-delay="0.5s">
+                            <div class="row">
+                                <div class="form-group col-md-6 mb-4">
+                                    <input type="text" name="name" class="form-control" id="fname" placeholder="First Name" required>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+
+                                <div class="form-group col-md-6 mb-4">
+                                    <input type="text" name="name" class="form-control" id="lname" placeholder="Last Name" required>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+
+                                <div class="form-group col-md-6 mb-4">
+                                    <input type="email" name ="email" class="form-control" id="email" placeholder="Email Address" required>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+
+                                <div class="form-group col-md-6 mb-4">
+                                    <input type="text" name="phone" class="form-control" id="phone" placeholder="Phone No" required>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+
+                                <div class="form-group col-md-12 mb-4">
+                                    <textarea name="msg" class="form-control" id="msg" rows="5" placeholder="Message" required></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+
+                                <div class="col-lg-12">
+                                    <div class="contact-form-btn">
+                                        <button type="submit" class="btn-default">submit message</button>
+                                        <div id="msgSubmit" class="h3 hidden"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- Contact Form End -->
+                </div>
+            </div>
         </div>
-    @endif
+    </div>
+    <!-- Page Contact Us End -->
 
-    <form wire:submit.prevent="submitForm">
-        <div class="row">
-            <div class="form-group col-md-6 mb-4">
-                <input type="text" class="form-control" wire:model.lazy="name" placeholder="First Name">
-                @error('name') <span class="text-danger">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="form-group col-md-6 mb-4">
-                <input type="email" class="form-control" wire:model.lazy="email" placeholder="Email Address">
-                @error('email') <span class="text-danger">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="form-group col-md-12 mb-4">
-                <input type="text" class="form-control" wire:model.lazy="subject" placeholder="Sujet">
-                @error('subject') <span class="text-danger">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="form-group col-md-12 mb-4">
-                <textarea class="form-control" rows="5" wire:model.lazy="message" placeholder="Message"></textarea>
-                @error('message') <span class="text-danger">{{ $message }}</span> @enderror
-            </div>
-
-            <div class="col-lg-12">
-                <button type="submit" class="btn btn-primary" wire:loading.attr="disabled">
-                    <span wire:loading.remove>Envoyer</span>
-                    <span wire:loading>Envoi...</span>
-                </button>
+    <!-- Google Map Start -->
+	<div class="google-map">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-12">
+                    <!-- Google Map Iframe Start -->
+                    <div class="google-map-iframe">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96737.10562045308!2d-74.08535042841811!3d40.739265258395164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1703158537552!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <!-- Google Map Iframe End -->
+                </div>
             </div>
         </div>
-    </form>
+    </div>
+	<!-- Google Map End -->
 </div>
